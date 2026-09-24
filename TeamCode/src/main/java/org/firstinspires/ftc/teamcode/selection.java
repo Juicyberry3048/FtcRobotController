e7c2fb1cde18f4076c54cfa19773d6b4c6b0a87f
Alex <ander.ander.grove@gmail.com>
2026-09-24 17:25:11 -0500

    Create selection.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/selection.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class selection extends OpMode {
+    @Override
+    public void init(){
+
+    }
+    @Override
+    public void loop(){
+        boolean aButton = gamepad1.a;
+        boolean bButton = gamepad1.b;
+        if(aButton && bButton){
+            telemetry.addData("A+B buttons", "Pressed!");
+        }
+        else if (aButton){
+            telemetry.addData("A button", "Pressed!");
+        }
+        else if (bButton){
+            telemetry.addData("B button", "Pressed!");
+        }
+        else{
+            telemetry.addData("No button", "Pressed :(");
+        }
+    }
+}
+/*
+every conditional operator / combination
+== - equal to
+!= - not equal to
+< - less than
+> - greater than
+<= - less than or equal to
+>= - greater than or equal to
+&& - AND
+|| - OR
+! - NOT
+^ - XOR (exclusive or)
+ */
